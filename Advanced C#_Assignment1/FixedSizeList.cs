1b972763a386e791b1e788e5070496a1465f9126
Lojain <logyemad2125@gmail.com>
2025-01-31 11:25:57 +0200

    Edit Something

---
+++ Advanced C#_Assignment1/FixedSizeList.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Advanced_C__Assignment1
+{
+    internal class FixedSizeList<T>
+    {
+        private List<T> list;
+        private int capacity;
+        public FixedSizeList(int cap)
+        {
+            if (cap <= 0)
+            {
+                throw new ArgumentException("Capacity must be greater than 0");
+            }
+
+            capacity = cap;
+            list = new List<T>(cap);
+        }
+        public int Count
+        {
+            get { return list.Count; }
+        }
+        public int Capacity
+        {
+            get { return capacity; }
+        }
+        public void Add(T item)
+        {
+            if (list.Count >= capacity)
+            {
+                throw new InvalidOperationException("the list is full cannot add more items");
+            }
+
+            list.Add(item);
+        }
+        public T Get(int index)
+        {
+            if (index < 0 || index >= list.Count)
+            {
+                throw new ArgumentOutOfRangeException("this is Invalid index");
+            }
+
+            return list[index];
+        }
+        public void Display()
+        {
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+      
+    }
+}
--- Advanced C#_Assignment1/Program.cs
+++ Advanced C#_Assignment1/Program.cs
@@ -6,32 +6,52 @@ namespace Advanced_C__Assignment1
     {
 
         #region function for Q2
-        public static void Reverse(ArrayList list)
-        {
-            int L= 0;
-            int R = list.Count - 1;
-            while (L < R)
-            {
-                object temp = list[L];
-                list[L] = list[R];
-                list[R] = temp;
-                L++;
-                R--;
-            }
-        }
+        //public static void Reverse(ArrayList list)
+        //{
+        //    int L= 0;
+        //    int R = list.Count - 1;
+        //    while (L < R)
+        //    {
+        //        object temp = list[L];
+        //        list[L] = list[R];
+        //        list[R] = temp;
+        //        L++;
+        //        R--;
+        //    }
+        //}
         #endregion
         #region function for Q3
-        public static List<int> EvenNumber(List<int> nums)
+        //public static List<int> EvenNumber(List<int> nums)
+        //{
+        //    List<int> evenNums = new List<int>();
+        //    foreach (var num in nums)
+        //    {
+        //        if (num % 2 == 0)
+        //        {
+        //            evenNums.Add(num);
+        //        }
+        //    }
+        //    return evenNums;
+        //}
+        #endregion
+        #region function for Q5
+        public static int NonRepeated(string s)
         {
-            List<int> evenNums = new List<int>();
-            foreach (var num in nums)
+            Dictionary<char,int> charCount = new Dictionary<char, int>();
+            foreach (char c in s)
             {
-                if (num % 2 == 0)
-                {
-                    evenNums.Add(num);
-                }
+                if (charCount.ContainsKey(c))
+                    charCount[c]++;
+                else
+                    charCount[c] = 1;
             }
-            return evenNums;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (charCount[s[i]] == 1)
+                    return i; 
+            }
+
+            return -1;
         }
         #endregion
         static void Main(string[] args)
@@ -77,37 +97,74 @@ namespace Advanced_C__Assignment1
             //Console.WriteLine(range.GetLength());
             #endregion
             #region Question2
-            ArrayList list1 = new ArrayList();
-            list1.Add("A");
-            list1.Add("B");
-            list1.Add("#");
-            list1.Add("R");
-            list1.Add(4);
+            //ArrayList list1 = new ArrayList();
+            //list1.Add("A");
+            //list1.Add("B");
+            //list1.Add("#");
+            //list1.Add("R");
+            //list1.Add(4);
 
-            Console.WriteLine("Original ArrayList: ");
-            foreach (var item in list1)
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
-            Reverse(list1);
-            Console.WriteLine("Reversed ArrayList: ");
-            foreach (var item in list1)
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
+            //Console.WriteLine("Original ArrayList: ");
+            //foreach (var item in list1)
+            //{
+            //    Console.Write(item + " ");
+            //}
+            //Console.WriteLine();
+            //Reverse(list1);
+            //Console.WriteLine("Reversed ArrayList: ");
+            //foreach (var item in list1)
+            //{
+            //    Console.Write(item + " ");
+            //}
+            //Console.WriteLine();
             #endregion
             #region Question3
-            List<int> nums = new List<int> { 2, 3,9, 5 ,4};
+            //List<int> nums = new List<int> { 2, 3,9, 5 ,4};
 
-            List<int> evenNums = EvenNumber(nums);
-            Console.WriteLine("even numbers:");
-            foreach (var number in evenNums)
-            {
-                Console.Write(number + " ");
-            }
-            Console.WriteLine();
+            //List<int> evenNums = EvenNumber(nums);
+            //Console.WriteLine("even numbers:");
+            //foreach (var number in evenNums)
+            //{
+            //    Console.Write(number + " ");
+            //}
+            //Console.WriteLine();
+            #endregion
+            #region Question 4
+            //try
+            //{
+            //    FixedSizeList<int> list = new FixedSizeList<int>(3);
+            //    list.Add(1);
+            //    list.Add(2);
+            //    list.Add(3);
+            //    Console.WriteLine("Current List id:");
+            //    list.Display();
+            //    list.Add(4);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //try
+            //{
+            //    FixedSizeList<string> stringList = new FixedSizeList<string>(2);
+            //    stringList.Add("Lojain");
+            //    stringList.Add("Emad");
+            //    Console.WriteLine("string List elements:");
+            //    stringList.Display();
+            //    Console.WriteLine(stringList.Get(5));
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            #endregion
+            #region Question5
+            string name = "Lojain";
+            int index = NonRepeated(name);
+            Console.WriteLine($"The index of the first NonRepeated character is:{index}");
+            string name2 = "salsabil";
+            int index2 = NonRepeated(name2);
+            Console.WriteLine($"The index of the first NonRepeated character is:{index2}");
             #endregion
 
         }
